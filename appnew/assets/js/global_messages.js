00eafba6e4e016bdc7fb54672f14589abe187bf1
gic-chandrashekhar <89963131+gic-chandrashekhar@users.noreply.github.com>
2022-12-02 10:05:56 +0530

    Dynamic fetch the json value and print only value this json

--- appnew/assets/js/global_messages.js
+++
@@ -1,5 +0,0 @@
-var global_messages =
-{
-	modulepermdenied:"You are not allowed to access the feature ",
-	licenseexpired:"License expired. Contact your administrator."
-};
--- appnew/assets/js/payment/pay_variable.js
+++
@@ -1,7 +0,0 @@
-var plan_Selected = 1;
-var mode_selected = 2;
-var max_users = 20;
-var currency = "";
-var priceanual = "";
-var pricemonth = "";
-var paydetails = "";
